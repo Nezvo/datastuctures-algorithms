2192402f1f5c3207b4478b1c0e40fb3ff3db2249
Zvonimir Korajlija <zkorajlija@erecruit.com>
2020-04-23 15:54:36 +0200

    Added priority queue implementation using heap

---
+++ DataStructures/PriorityQueueWithHeap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DataStructures
+{
+	public class PriorityQueueWithHeap<T> where T : IComparable
+	{
+		private Heap<T> heap;
+
+		public PriorityQueueWithHeap(int size)
+		{
+			heap = new Heap<T>(size);
+		}
+
+		public void Enqueue(T item)
+		{
+			heap.Add(item);
+		}
+
+		public T Dequeue()
+		{
+			return heap.Remove();
+		}
+
+		public bool isEmpty() => heap.IsEmpty();
+	}
+}
