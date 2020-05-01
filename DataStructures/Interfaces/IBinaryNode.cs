737cd3bf42b56b5c407a61d6467ed095b754c652
Zvonimir Korajlija <zvonimir.korajlija@gmail.com>
2020-05-01 22:59:30 +0200

    added type constraint

--- DataStructures/Interfaces/IBinaryNode.cs
+++ DataStructures/Interfaces/IBinaryNode.cs
@@ -1,6 +1,8 @@
-﻿namespace DataStructures.Interfaces
+﻿using System;
+
+namespace DataStructures.Interfaces
 {
-	public interface IBinaryNode<T>
+	public interface IBinaryNode<T> where T : IComparable
 	{
 		T Value { get; set; }
 		int Height { get; set; }
